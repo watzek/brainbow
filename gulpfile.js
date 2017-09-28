4f9f19a329e635b6a114a4ba7e94a824391dc4a8
budak <thatbudakguy@gmail.com>
2017-09-28 11:20:04 -0700

    (#1) Setup development environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
 const gulp        = require('gulp')
-const browserSync = require('browser-sync').create()
 const sass        = require('gulp-sass')
+const concat      = require('gulp-concat')
+const rename      = require('gulp-rename')
+const uglify      = require('gulp-uglify')
+const uglifyCss   = require('gulp-uglifycss')
+const browserSync = require('browser-sync').create()
 
-gulp.task('serve', ['sass'], () => {
+gulp.task('serve', ['sass', 'js'], () => {
   browserSync.init({
     proxy: "localhost:8080"
   })
   gulp.watch('theme/sass/**/*.scss', ['sass'])
+  gulp.watch('theme/js/**/*.js', ['js'])
   gulp.watch('theme/**/*.php').on('change', browserSync.reload)
 })
 
 gulp.task('sass', () => {
   return gulp.src('theme/sass/**/*.scss')
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('theme'))
     .pipe(browserSync.stream())
+    .pipe(uglifyCss())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('theme'))
+})
+
+gulp.task('js', () => {
+  return gulp.src('theme/js/**/*.js')
+    .pipe(concat('brainbow.js'))
+    .pipe(gulp.dest('theme'))
+    .pipe(browserSync.stream())
+    .pipe(rename('brainbow.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('theme'))
 })
 
 gulp.task('default', ['serve'])
--- theme/functions.php
+++ theme/functions.php
@@ -4,8 +4,7 @@
  * Scripts & Styles
  */
 
-function brainbow_enqueue_styles()
-{
+function brainbow_enqueue_styles() {
     $parent_style = 'twentysixteen-style';
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style',
@@ -16,4 +15,9 @@ function brainbow_enqueue_styles()
     wp_enqueue_style('mirador', get_stylesheet_directory_uri() . '/mirador/css/mirador-combined.css');
 }
 
+function brainbow_enqueue_scripts() {
+    wp_enqueue_script('brainbow-js', get_stylesheet_directory_uri() . '/brainbow.js');
+}
+
 add_action('wp_enqueue_scripts', 'brainbow_enqueue_styles');
+add_action('wp_enqueue_scripts', 'brainbow_enqueue_scripts');
