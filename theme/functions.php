637ac24fe52539ef55ab2f4b89da151b61aed3b8
budak <thatbudakguy@gmail.com>
2017-11-29 08:57:44 -0800

    use https UV link; remove mirador js

--- theme/functions.php
+++ theme/functions.php
@@ -7,7 +7,6 @@
 function brainbow_enqueue_styles() {
     $parent_style = 'twentysixteen-style';
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('mirador', get_stylesheet_directory_uri() . '/vendor/mirador/css/mirador-combined.css');
     wp_enqueue_style('child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
@@ -18,17 +17,8 @@ function brainbow_enqueue_styles() {
 function brainbow_enqueue_scripts() {
     wp_deregister_script('jquery'); // don't use built-in jquery
     wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.1.1.min.js'); // use our own version of jquery
-    wp_enqueue_script('mirador', get_stylesheet_directory_uri() . '/vendor/mirador/mirador.js');
     wp_enqueue_script('brainbow-js', get_stylesheet_directory_uri() . '/brainbow.js');
 }
 
 add_action('wp_enqueue_scripts', 'brainbow_enqueue_styles');
 add_action('wp_enqueue_scripts', 'brainbow_enqueue_scripts');
-
-// remove JQMigrate as mirador has it?
-add_action('wp_default_scripts', function ($scripts) {
-    if (! empty($scripts->registered['jquery'])) {
-        $jquery_dependencies = $scripts->registered['jquery']->deps;
-        $scripts->registered['jquery']->deps = array_diff($jquery_dependencies, array( 'jquery-migrate' ));
-    }
-});
--- theme/viewer.php
+++ theme/viewer.php
@@ -11,35 +11,6 @@ global $post;
 $image_title = $post->post_name;
 ?>
 
-<!-- <div class="mirador-wrapper">
-    <div id="viewer"></div>
-    <script type="text/javascript">
-    Mirador({
-      id: "viewer",
-      buildPath: "<?= get_stylesheet_directory_uri(); ?>/mirador/",
-      data: [{
-          "manifestUri": "<?php the_title(); ?>",
-          "location": "Lewis & Clark College"
-      }],
-      mainMenuSettings: {
-          show: false,
-      },
-      saveSession: true,
-      windowObjects: [
-          {
-              loadedManifest: "<?php the_title(); ?>",
-              viewType: "ImageView",
-              availableViews: ["ImageView"],
-              bottomPanel: false,
-              sidePanel: false,
-              annotationLayer: true,
-              annotationCreation: false,
-              annotationState: 'annoOnCreateOff'
-          }
-      ]
-    });
-    </script>
-</div> -->
 <div class="uv"
      data-locale="en-GB:English (GB),cy-GB:Cymraeg"
      data-config="/config.json"
@@ -52,12 +23,7 @@ $image_title = $post->post_name;
      data-rotation="0"
      style="height:777px; background-color: #000">
  </div>
-<script type="text/javascript" id="embedUV" src="http://universalviewer.io/uv/lib/embed.js"></script>
+<script type="text/javascript" id="embedUV" src="https://universalviewer.io/uv/lib/embed.js"></script>
 <script type="text/javascript">/* wordpress fix */</script>
-<!-- <div id="controls">
-    <button>View details</button>
-    <button>Toggle annotations</button>
-    <a href="/request?image=<?= $image_title; ?>"><button>Request this image</button></a>
-</div> -->
 
 <?php get_footer(); ?>
